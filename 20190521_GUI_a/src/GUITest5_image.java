088a1bcd7ae770feed15a4742ac9222dc3808902
Park Mincho <s2018s28@e-mirim.hs.kr>
2019-05-21 19:39:22 +0900

    ImageIcon

--- 20190521_GUI_a/src/GUITest5_image.java
+++ 20190521_GUI_a/src/GUITest5_image.java
@@ -15,16 +15,18 @@ public class GUITest5_image {
 		frame.setPreferredSize(new Dimension(500, 333));
 		
 		//ImageIcon, JLabel
-		ImageIcon EXO = new ImageIcon("src/images/첸1.jpg");
-		JLabel imageLable = new JLabel(EXO);
+		ImageIcon chen = new ImageIcon("src/images/첸1.jpg");
+		JLabel imageLable = new JLabel(chen);
 		
 		//ImageIcon, JButton
 		ImageIcon chanyeol = new ImageIcon("src/images/찬열.jpeg");
 		
 		//ImageIcon 크기 수정
 		ImageIcon smallChanyeol = new ImageIcon(chanyeol.getImage().getScaledInstance(150, 150, Image.SCALE_DEFAULT));
-		
 		JButton imageButton = new JButton(chanyeol);
+		imageButton.setRolloverIcon(chen); //마우스 올리면
+		imageButton.setPressedIcon(chanyeol); //마우스 클릭하면
+		
 		
 		panel.add(imageButton);
 		panel.add(imageLable);
