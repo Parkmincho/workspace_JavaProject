ce9d5ef6b7633cde147adebc0e1cd2a7e0a2c2cc
Park Mincho <s2018s28@e-mirim.hs.kr>
2019-06-04 18:20:45 +0900

    event

---
+++ 20190604_GUI/src/IndepClassListener.java
@@ -0,0 +1,40 @@
+import java.awt.*;
+import java.awt.ActiveEvent.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class IndepClassListener extends JFrame {
+
+
+	IndepClassListener() {
+		setTitle("Action 이벤트 리스너 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		JButton btn = new JButton("Action");
+		btn.addActionListener(new MyActionListener());
+		c.add(btn);
+		setSize(250, 120);
+		setVisible(true);
+	}
+
+
+	//내부클래스로 Action 리스너를 작성한다.
+	class MyActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			JButton b = (JButton)e.getSource();
+			if(b.getText().contentEquals("Action"))
+				b.setText("액션");
+			else
+				b.setText("Action");
+
+			//InnerClassListener나 JFrame의 멤버호출 가능
+			setTitle(b.getText());
+		}
+	}
+	public static void main(String[] args) {
+		new IndepClassListener();
+	}
+}
