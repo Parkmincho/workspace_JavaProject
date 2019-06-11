b5a55f9e94270bf5fdfb164b1f49202e888c6d23
Park Mincho <s2018s28@e-mirim.hs.kr>
2019-06-11 10:10:38 +0900

    .

--- 1년 Project/src/GUI.java
+++ 1년 Project/src/GUI.java
@@ -1,8 +1,30 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class GUI {
 
 	public static void main(String[] args) {
+		JFrame frame = new JFrame("급식");
+		JPanel panel = new JPanel();
+		frame.setPreferredSize(new Dimension(700, 500));
 		
+		JButton beakground = new JButton();
+		
+		
+		panel.add(beakground);
+		frame.add(panel);
+		
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
---
+++ 1년 Project/src/schoolmeals.java
@@ -0,0 +1,8 @@
+
+public class schoolmeals {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
